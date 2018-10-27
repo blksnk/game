067278fd25d2449b9008d7b3fb2ef129c3cb8f16
JN Veigel <art.jnveigel@gmail.com>
2018-10-27 09:46:29 +0200

    basic playground, x-y movement and rotation around center of self

---
+++ js/script.js
@@ -0,0 +1,86 @@
+var canvas = document.querySelector(".game");
+
+var ctx = canvas.getContext("2d");
+
+var carImg = new Image();
+carImg.src = "./images/car1.png";
+
+var car = {
+	x: 500,
+	y: 500,
+	width: 57,
+	height: 81,
+	isCrashed: false,
+	rotation: 0,
+	rotate: function (){
+		return this.rotation * Math.PI/180;
+	},
+	drawMe: function () {
+
+		ctx.translate(this.x+this.width/2, this.y+this.height/2)
+		ctx.rotate(this.rotate());
+
+		ctx.drawImage(carImg, -this.width/2, -this.height/2, this.width, this.height);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+drawingLoop();
+
+function drawingLoop () {
+
+	ctx.clearRect(0, 0, 1000, 1000)
+	
+	ctx.lineWidth = 4;
+	ctx.strokeRect(0, 0, 1000, 1000);
+
+	ctx.save();
+
+
+	car.drawMe();
+
+
+	ctx.restore();
+
+	requestAnimationFrame(function () {
+		//set up a recursive loop : the function call itself
+		drawingLoop();
+	});
+}
+
+document.onkeydown = function (event) {
+
+	if (!car.isCrashed) {
+		switch (event.keyCode) {
+			case 90: //z
+				car.y -= 10;
+				break;
+
+			case 83: //s
+				car.y += 10;
+				break;
+
+			case 81: //q
+				
+				car.rotation -=10;
+				break;
+
+			case 68: //d
+				
+				car.rotation +=10;
+				break;
+
+		};
+	}
+}
